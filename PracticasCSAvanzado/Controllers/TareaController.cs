ca5f976444e5bde1b97398f79466df7b4d3f01b9
AJ-Paulino <ajpaulino38@hotmail.com>
2025-05-08 12:14:26 -0400

    Repositorio nuevo

---
+++ PracticasCSAvanzado/Controllers/TareaController.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationLayer.Services.TareaServices;
+using DomainLayer.Models;
+using DomainLayer.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticasCSAvanzado.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        private readonly TareaService _service;
+        public TareaController(TareaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response<Tarea>>> GetTaskAllAsync()
+            => await _service.GetTaskAllAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Response<Tarea>>> GetTaskByIdAllAsync(int id)
+            => await _service.GetTaskByIdAllAsync(id);
+
+        [HttpPost]
+        public async Task<ActionResult<Response<string>>> AddTaskAllAsync(Tarea tarea)
+            => await _service.AddTaskAllAsync(tarea);
+
+        [HttpPut]
+        public async Task<ActionResult<Response<string>>> UpdateTaskAllAsync(Tarea tarea)
+            => await _service.UpdateTaskAllAsync(tarea);
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Response<string>>> DeleteTaskAllAsync(int id)
+            => await _service.DeleteTaskAllAsync(id);
+    }
+}
---
+++ PracticasCSAvanzado/Program.cs
@@ -0,0 +1,52 @@
+using ApplicationLayer.Services.TareaServices;
+using DomainLayer.Models;
+using InfraestructureLayer.Context;
+using InfraestructureLayer.Repositorio.Commons;
+using InfraestructureLayer.Repositorio.TareaRepositorio;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Conexión a la base de datos
+builder.Services.AddDbContext<PracticasCSAvanzadoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PracticasCSADB"))
+);
+
+builder.Services.AddScoped<ICommonsProcess<Tarea>, TareaRepositorio>();
+builder.Services.AddScoped<TareaService>();
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+//Migraciones de la base de datos
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var context = scope.ServiceProvider.GetRequiredService<PracticasCSAvanzadoContext>();
+//    context.Database.Migrate();
+//}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "PEC");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
