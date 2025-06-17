0f93f01dd06177c2b008204e5553ed0a521d332b
AJ-Paulino <ajpaulino38@hotmail.com>
2025-06-17 11:21:12 -0400

    .

---
+++ InfraestructureLayer/Migrations/20250617151926_CambiarTipoDato.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraestructureLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiarTipoDato : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Password",
+                table: "Usuarios",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
