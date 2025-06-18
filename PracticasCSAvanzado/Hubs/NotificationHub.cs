76cf0b52947207eecc2d61867c8a03aebee597fa
AJ-Paulino <ajpaulino38@hotmail.com>
2025-06-18 09:31:58 -0400

    .

---
+++ PracticasCSAvanzado/Hubs/NotificationHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PracticasCSAvanzado.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task EnviarNotificacion(string mensaje)
+            => await Clients.All.SendAsync("RecibirNotificacion", mensaje);
+    }
+}
